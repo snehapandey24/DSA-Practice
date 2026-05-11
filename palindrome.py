6776c3072d18fb53a25aee378ad879b7ec29cfa7
Sneha Pandey <snehapandey2410@gmail.com>
2026-05-11 20:21:11 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+n = "madam"
+
+rev = n[::-1]
+
+if n == rev:
+    print(True)
+
+else:
+    print(False)
+        
---
+++ reverse_a_string.py
@@ -0,0 +1,7 @@
+s="hello" 
+rev=""
+
+for i in range(len(s)-1,-1,-1):
+        rev= rev + s[i]
+
+print(rev)    
